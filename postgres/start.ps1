cbfbac8a332b229f370b12b81c1f7d0113c26d65
Joseph Stoner <jsstoner6@gmail.com>
2026-05-27 06:55:53 -0400

    add data creation scripts

---
+++ postgres/start.ps1
@@ -0,0 +1,21 @@
+﻿# ==========================================
+# PostgreSQL Background Startup Script
+# Save as: start-postgres.ps1
+# ==========================================
+
+$PgBin = "C:\Program Files\PostgreSQL\18\bin\"
+$PgData = "C:\Program Files\PostgreSQL\18\data"
+
+$PostgresExe = Join-Path $PgBin "postgres.exe"
+
+Write-Host ""
+Write-Host "Starting PostgreSQL in background..."
+Write-Host ""
+
+Start-Process `
+    -FilePath $PostgresExe `
+    -ArgumentList "-D `"$PgData`"" `
+    -WorkingDirectory $PgBin
+
+Write-Host "PostgreSQL launch command issued."
+Write-Host ""
